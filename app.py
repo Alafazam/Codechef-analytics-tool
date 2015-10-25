6b6f0f895bc6a98e0940f806a938e9da15f31071
alafazam <alafazam@gmail.com>
2015-10-25 23:41:08 +0530

    fix end message

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def get_data(username):
 			pagez ='request fetched upto '+str(pagez)+' page'
 			yield ','+str(pagez)+ '\n'
 		else:
-			yield 'total'+str(pagez)+ 'out of '+str(number_of_pages)+' \n'
+			yield 'total '+str(pagez)+ 'out of '+str(number_of_pages-1)+' \n'
 	return Response(generate(number_of_pages), mimetype='text/json')
 	# return render_template('result.html',data=json.dumps(obj_data),pagez=pagez)
 
