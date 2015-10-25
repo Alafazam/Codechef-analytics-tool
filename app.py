4ae8cb8caa5503466384d64506b16f9a75bc330b
alafazam <alafazam@gmail.com>
2015-10-25 15:39:50 +0530

    add

--- app.py
+++ app.py
@@ -71,6 +71,6 @@ def get_data(username):
 	
 	return render_template('result.html',data=json.dumps(obj_data))
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port,debug=True)
