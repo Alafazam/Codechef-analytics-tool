81d5eb22b0041cc88d06541fb797ea58c6fdc2c2
alafazam <alafazam@gmail.com>
2015-10-30 01:30:09 +0530

    app

--- app.py
+++ app.py
@@ -44,79 +44,82 @@ def get_data(username):
 
 
 
-# @app.route('/data', methods=['GET'])
-# def datadiplay():
-# 	time_data = []
-# 	data_hours = []
-# 	data_mins = []
-# 	pagez=0
-# 	io = StringIO()
-# 	username = 'alafazam'
-# 	# /^[a-z]{1}[a-z0-9_]{3,13}$/
-# 	if re.match('^[a-z]{1}[a-z0-9_]{3,13}$',username)==None:
-# 		return "Invalid username"
-# 	s = requests.Session()
+@app.route('/data', methods=['GET'])
+def datadiplay():
+	time_data = []
+	data_hours = []
+	data_mins = []
+	pagez=0
+	# io = StringIO()
+	username = 'alafazam'
+	# /^[a-z]{1}[a-z0-9_]{3,13}$/
+	if re.match('^[a-z]{1}[a-z0-9_]{3,13}$',username)==None:
+		return "Invalid username"
+	s = requests.Session()
 
-# 	print 'looking for ' + username
-# 	url = 'https://www.codechef.com/recent/user/page=1&user_handle='+str(username)
-# 	r = s.get(url).json()
-# 	number_of_pages = r["max_page"]
+	print 'looking for ' + username
+	url = 'https://www.codechef.com/recent/user/page=1&user_handle='+str(username)
+	r = s.get(url).json()
+	number_of_pages = r["max_page"]
 
-# 	if number_of_pages==0:
-# 		return 'This user does not exist'
+	if number_of_pages==0:
+		return 'This user does not exist'
 
-# 	print 'max pages are ' + str(number_of_pages)
-# 	# def generate(number_of_pages):
-# 	# yield 'Loading....'
-# 	try:	
-# 		for x in range(0,number_of_pages):
-# 			obj_data = []
-# 			time.sleep(0.5)
-# 			print "sending request for page "+ str(x) 
-# 			url = 'https://www.codechef.com/recent/user?page='+str(x)+'&user_handle='+str(username)
-# 			r = s.get(url).json()
-# 			e = r["content"]
-# 			soup = BeautifulSoup(e, 'html.parser')
-# 			trs = soup.find_all('tr','kol')
-# 			pagez = x
-# 			for q in trs:
-# 				times = str(q.contents[0].text)
-# 				# problem_code = str(q.contents[1].a['href']).split('/')[-1]
-# 				# status = str(q.contents[2].span['title'])
-# 				# lang = str(q.contents[31].text)
-# 				# obj = {'time':times,'problem_code':problem_code,'status':status,'lang':lang}
-# 				# obj_data.append(obj)
-# 				time_data.append(times)
-# 				hours_search = re.search(pm,times)
-# 				if hours_search:
-# 					hours = hours_search.group(1)
-# 					mins = hours_search.group(2)
-# 					if hours_search.group(3)=='AM':
-# 						data_hours.append(int(hours))
-# 					else:
-# 						data_hours.append(int(hours)+12)
-# 					data_mins.append(int(mins))
-# 			print "page "+str(x)+" done"
-# 			# yield str(obj_data)+ '\n,'
-# 	except :
-# 		print "error occured saving data recieved uptill now"
-# 		print 'request fetched upto '+str(pagez)+' page'
-# 		print ','+str(pagez)+ '\n'
-# 	else:
-# 		print 'total '+str(pagez)+ 'out of '+str(number_of_pages-1)+' \n'
+	print 'max pages are ' + str(number_of_pages)
+	def generate(number_of_pages):
+		yield 'Loading....'
+		try:	
+			for x in range(0,number_of_pages):
+				obj_data = []
+				time.sleep(0.5)
+				print "sending request for page "+ str(x) 
+				url = 'https://www.codechef.com/recent/user?page='+str(x)+'&user_handle='+str(username)
+				r = s.get(url).json()
+				e = r["content"]
+				soup = BeautifulSoup(e, 'html.parser')
+				trs = soup.find_all('tr','kol')
+				pagez = x
+				for q in trs:
+					times = str(q.contents[0].text)
+					# problem_code = str(q.contents[1].a['href']).split('/')[-1]
+					# status = str(q.contents[2].span['title'])
+					# lang = str(q.contents[31].text)
+					# obj = {'time':times,'problem_code':problem_code,'status':status,'lang':lang}
+					# obj_data.append(obj)
+					# time_data.append(times)
+					hours_search = re.search(pm,times)
+					if hours_search:
+						hours = hours_search.group(1)
+						print hours
+						mins = hours_search.group(2)
+						obja = {'h':hours,'min':mins}
+						# if hours_search.group(3)=='AM':
+						# 	data_hours.append(int(hours))
+						# else:
+						# 	data_hours.append(int(hours)+12)
+						# data_mins.append(int(mins))
+						yield str(obja)
+				print "page "+str(x)+" done"
 
-# 	x = np.asarray(data_hours)
-# 	y = np.asarray(data_mins)
-# 	sns.jointplot(x, y, kind="hex", color="#4CB391")
-# 	# fig.savefig(io, format='png')
-# 	sns.plt.savefig(io, format='png')
-# 	data = io.getvalue().encode('base64')
-# 	return render_template('result.html',data=data)
+		except :
+			print "error occured saving data recieved uptill now"
+			print 'request fetched upto '+str(pagez)+' page'
+			print ','+str(pagez)+ '\n'
+		else:
+			print 'total '+str(pagez)+ 'out of '+str(number_of_pages-1)+' \n'
+
+	# x = np.asarray(data_hours)
+	# y = np.asarray(data_mins)
+	# sns.jointplot(x, y, kind="hex", color="#4CB391")
+	# # fig.savefig(io, format='png')
+	# sns.plt.savefig(io, format='png')
+	# data = io.getvalue().encode('base64')
+	# return render_template('result.html',data=data)
 
 
-	# return Response(generate(number_of_pages))
-	# return render_template('result.html',data=json.dumps(obj_data),pagez=pagez)
+	return Response(generate(number_of_pages))
+	return render_template('result.html',data=json.dumps(obj_data),pagez=pagez)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port,debug=True)
