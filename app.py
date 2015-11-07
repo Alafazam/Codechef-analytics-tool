fbea2da40529d7742239118085094ed5c0783143
alafazam <alafazam@gmail.com>
2015-11-08 02:35:24 +0530

    ignoring recent submissions

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def generate__data(username):
 			soup = BeautifulSoup(e, 'html.parser')
 			trs = soup.find_all('tr','kol')
 			for q in trs:
-				response_string=''
+				response_string = ''
 				times = str(q.contents[0].text)
 				search_grp = re.search(pm,times)
 				if search_grp:
@@ -92,15 +92,12 @@ def generate__data(username):
 					date_d = search_grp.group(4)
 					date_m = search_grp.group(5)
 					date_y = search_grp.group(6)
-					response_string +=',{ "hours":"' + str(hours+(12*int(am_pm=='PM'))) + '", "minutes":"'+ mins + '", "date_d":"' + date_d+ '", "date_y":"' + date_y+ '", "date_m":"' + date_m + '"' 
-				
-				problem_code = str(q.contents[1].a['href']).split('/')[-1]
-				qstatus = str(q.contents[2].span['title']) if q.contents[2].span['title'] else 'None'
-				langz = str(q.contents[3].text)
-				response_string += ', "problem_code":"'+ problem_code + '", "qstatus":"'+ qstatus + '", "langz":"'+ langz+ '" } '	
+					response_string +=',{ "hours":"' + str(hours+(12*int(am_pm=='PM'))) + '", "minutes":"'+ mins + '", "date_d":"' + date_d+ '", "date_y":"' + date_y+ '", "date_m":"' + date_m + '"' 			
+					problem_code = str(q.contents[1].a['href']).split('/')[-1]
+					qstatus = str(q.contents[2].span['title']) if q.contents[2].span['title'] else 'None'
+					langz = str(q.contents[3].text)
+					response_string += ', "problem_code":"'+ problem_code + '", "qstatus":"'+ qstatus + '", "langz":"'+ langz+ '" } '	
 				yield ''+ response_string
-
-		# yield "   end="+str(datetime.datetime.now())+''
 		yield " ] }"
 	return Response(generate())
 
